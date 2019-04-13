3c3bbb3fc7e6e910943fcd5be6b0b2382b80b982
wei feng <theweifeng@gmail.com>
2019-04-13 17:40:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,12 +7,9 @@ def key_for_min_value(name_hash)
   min_val = 0
   min_key = ""
   
-  name_hash.each do |key, val|
-    if val < min_val
-      min_val = val
-      min_key = key
-    end
-  end
+  array = name_hash.map
+  
+  
   
   min_key
 end
